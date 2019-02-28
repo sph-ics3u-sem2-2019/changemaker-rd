91c4240ea2f5dd999850b247d974bbbe8166ce22
daniadeleon <47532261+daniadeleon@users.noreply.github.com>
2019-02-28 11:15:26 -0500

    got rid of unnecessary change

--- src/Main.java
+++ src/Main.java
@@ -23,16 +23,81 @@ public class Main {
 		int num50s;
 		int num20s;
 		int num10s;
+		int num5s;
+		int num2s;
+		int num1s;
+		double quarters;
+		double dimes;
+		double nickels;
 		//How many 100 dollar bills?
 		System.out.println("Your change is "+change);
 		num100s=(int)(change/100);
 		change=change%100;
-		System.out.println(num100s+" x $100");
-		
+		if (num100s>0) {
+			System.out.println(num100s+" x $100");
+		}
+		//how many 50 dollar bills
 		num50s=(int)(change/50);
-		
-		
-		
-	}
+		change=change%50;
+		if (num50s>0) {
+		System.out.println(num50s+" x $50");
+		}
+		//how many 20 dollar bills
+		num20s=(int)(change/20);
+		change=change%20;
+		if (num20s>0) {
+		System.out.println(num20s+" x $20");
+		}
+		//how many ten dollar bills
+		num10s=(int)(change/10);
+		change=change%10;
+		if (num10s>0) {
+		System.out.println(num10s+" x $10");
+		}
+		//how many five dollar bills 
+		num5s=(int)(change/5);
+		change=change%5;
+		if (num5s>0) {
+		System.out.println(num5s+" x $5");
+		}
+		//how many toonies
+		num2s=(int)(change/2);
+		change=change%2;
+		if (num2s>0) {
+		System.out.println(num2s+" x $2");
+		}
+		//how many loonies
+		num1s=(int)(change/1);
+		change=change%1;
+		if (num1s>0) {
+		System.out.println(num1s+" x $1");
+		}
+		//how many quarters
+		quarters=(int)(change/0.25);
+		change=change%0.25;
+		if (quarters>0) {
+		System.out.println(quarters+" x $0.25");
+		}
+		//how many dimes
+		dimes=(int)(change/0.10);
+		change=change%0.10;
+		if (dimes>0) {
+		System.out.println(dimes+" x $0.10");
+		}
+		//how many nickels
+		nickels=(int)(change/0.05);
+		change=change%0.05;
+		if (nickels>0) {
 
+		}
+		if (change >=0.03) {
+			nickels=nickels+1;
+		}
+		System.out.println(nickels+" x $0.05"); 
+	}
 }
+		
+		
+	
+
+
